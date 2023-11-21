177b21c45c023203497235902f5fa2461de4b152
efraitor_ <efraitor.contact@gmail.com>
2023-11-21 10:31:21 +0100

    ScriptableObject
    Create ScriptableObjects

--- Assets/Code/Scripts/State.cs
+++ Assets/Code/Scripts/State.cs
@@ -12,7 +12,18 @@ public class State : ScriptableObject
     //[TextArea(,)] -> atributo que permite crear un cuadro de texto con la cantidad de líneas escalable
     //[SerializeField] -> atributo que permite que una variable o referencia siga siendo privada pero accesible desde el editor de Unity
     [TextArea(14, 10)] [SerializeField] string storyText;
+    //Array de estados a los que podemos ir desde el estado actual
+    [SerializeField] State[] nextStates;
 
-    //Hola que tal he hecho un cambio en un script
+    //Método que nos devuelve el contenido de la caja de texto del ScriptableObject
+    public string GetStateStoryText()
+    {
+        return storyText;
+    }
 
+    //Método que nos devuelve los estados a los que podemos ir desde el estado actual
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
 }
